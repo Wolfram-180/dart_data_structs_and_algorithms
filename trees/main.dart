00af31062f5761ce1005c18110d17c2a4a5dc933
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-06 16:59:04 +0300

    working on trees

---
+++ trees/main.dart
@@ -0,0 +1,11 @@
+import 'tree.dart';
+
+void main() {
+  void main() {
+    final beverages = TreeNode('Beverages');
+    final hot = TreeNode('Hot');
+    final cold = TreeNode('Cold');
+    beverages.add(hot);
+    beverages.add(cold);
+  }
+}
