f22d581c2f3e42377494d5abf1c6db69d82c04f8
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-06 11:23:58 +0300

    QueueRingBuffer done

--- queues/main.dart
+++ queues/main.dart
@@ -1,10 +1,23 @@
 import 'queue.dart';
 
 void main() {
+  /***
   final queue = QueueLinkedList<String>();
   queue.enqueue('Ray');
   queue.enqueue('Brian');
   queue.enqueue('Eric');
+  print(queue); // [Ray, Brian, Eric]
+  queue.dequeue();
+  print(queue); // [Brian, Eric]
+  var name = queue.peek;
+  print(name);
+  print(queue); // [Brian, Eric]
+***/
+
+  final queue = QueueRingBuffer<String>(10);
+  queue.enqueue("Ray");
+  queue.enqueue("Brian");
+  queue.enqueue("Eric");
   print(queue); // [Ray, Brian, Eric]
   queue.dequeue();
   print(queue); // [Brian, Eric]
--- queues/queue.dart
+++ queues/queue.dart
@@ -1,4 +1,5 @@
 import 'doubly_linked_list.dart';
+import 'ring_buffer.dart';
 
 abstract class Queue<E> {
   bool enqueue(E element);
@@ -49,4 +50,31 @@ class QueueLinkedList<E> implements Queue<E> {
 
   @override
   String toString() => _list.toString();
+}
+
+class QueueRingBuffer<E> implements Queue<E> {
+  QueueRingBuffer(int length) : _ringBuffer = RingBuffer<E>(length);
+
+  final RingBuffer<E> _ringBuffer;
+
+  @override
+  bool enqueue(E element) {
+    if (_ringBuffer.isFull) {
+      return false;
+    }
+    _ringBuffer.write(element);
+    return true;
+  }
+
+  @override
+  E? dequeue() => _ringBuffer.read();
+
+  @override
+  bool get isEmpty => _ringBuffer.isEmpty;
+
+  @override
+  E? get peek => _ringBuffer.peek;
+
+  @override
+  String toString() => _ringBuffer.toString();
 }
