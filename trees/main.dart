57c38d00c135a5d637ee0ef57a6c2727d127468c
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-08 09:11:50 +0300

    createBinaryTree

--- trees/main.dart
+++ trees/main.dart
@@ -1,36 +1,27 @@
 import 'tree.dart';
 import 'binary_node.dart';
 
-TreeNode<String> makeBeverageTree() {
-  final tree = TreeNode('beverages');
-  final hot = TreeNode('hot');
-  final cold = TreeNode('cold');
-  final tea = TreeNode('tea');
-  final coffee = TreeNode('coffee');
-  final chocolate = TreeNode('cocoa');
-  final blackTea = TreeNode('black');
-  final greenTea = TreeNode('green');
-  final chaiTea = TreeNode('chai');
-  final soda = TreeNode('soda');
-  final milk = TreeNode('milk');
-  final gingerAle = TreeNode('ginger ale');
-  final bitterLemon = TreeNode('bitter lemon');
-  tree.add(hot);
-  tree.add(cold);
-  hot.add(tea);
-  hot.add(coffee);
-  hot.add(chocolate);
-  cold.add(soda);
-  cold.add(milk);
-  tea.add(blackTea);
-  tea.add(greenTea);
-  tea.add(chaiTea);
-  soda.add(gingerAle);
-  soda.add(bitterLemon);
-  return tree;
+void main() {
+  final tree = createBinaryTree();
+  print(tree);
 }
 
-void main() {
+BinaryNode<int> createBinaryTree() {
+  final zero = BinaryNode(0);
+  final one = BinaryNode(1);
+  final five = BinaryNode(5);
+  final seven = BinaryNode(7);
+  final eight = BinaryNode(8);
+  final nine = BinaryNode(9);
+  seven.leftChild = one;
+  one.leftChild = zero;
+  one.rightChild = five;
+  seven.rightChild = nine;
+  nine.leftChild = eight;
+  return seven;
+}
+
+void main_non_binary() {
   final tree = makeBeverageTree();
 
   final searchResult1 = tree.search('ginger ale');
@@ -61,4 +52,33 @@ void main() {
       ),
     ],
   );
+}
+
+TreeNode<String> makeBeverageTree() {
+  final tree = TreeNode('beverages');
+  final hot = TreeNode('hot');
+  final cold = TreeNode('cold');
+  final tea = TreeNode('tea');
+  final coffee = TreeNode('coffee');
+  final chocolate = TreeNode('cocoa');
+  final blackTea = TreeNode('black');
+  final greenTea = TreeNode('green');
+  final chaiTea = TreeNode('chai');
+  final soda = TreeNode('soda');
+  final milk = TreeNode('milk');
+  final gingerAle = TreeNode('ginger ale');
+  final bitterLemon = TreeNode('bitter lemon');
+  tree.add(hot);
+  tree.add(cold);
+  hot.add(tea);
+  hot.add(coffee);
+  hot.add(chocolate);
+  cold.add(soda);
+  cold.add(milk);
+  tea.add(blackTea);
+  tea.add(greenTea);
+  tea.add(chaiTea);
+  soda.add(gingerAle);
+  soda.add(bitterLemon);
+  return tree;
 }
