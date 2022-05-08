2b536a2a4e39456856e9801af09697a644ea95af
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-08 12:36:58 +0300

    binary_search_tree.dart

--- trees/binary_node.dart
+++ trees/binary_node.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class BinaryNode<T> {
   BinaryNode(this.value);
   T value;
@@ -35,5 +37,32 @@ class BinaryNode<T> {
       '$bottom ',
     );
     return '$a$b$c';
+  }
+
+  void traverseInOrder(void Function(T value) action) {
+    leftChild?.traverseInOrder(action);
+    action(value);
+    rightChild?.traverseInOrder(action);
+  }
+
+  void traversePreOrder(void Function(T value) action) {
+    action(value);
+    leftChild?.traversePreOrder(action);
+    rightChild?.traversePreOrder(action);
+  }
+
+  void traversePostOrder(void Function(T value) action) {
+    leftChild?.traversePostOrder(action);
+    rightChild?.traversePostOrder(action);
+    action(value);
+  }
+
+  int height(BinaryNode? node) {
+    if (node == null) return -1;
+    return 1 +
+        max(
+          height(node.leftChild),
+          height(node.rightChild),
+        );
   }
 }
--- trees/main.dart
+++ trees/main.dart
@@ -2,11 +2,74 @@ import 'tree.dart';
 import 'binary_node.dart';
 
 void main() {
-  final tree = createBinaryTree();
-  print(tree);
+  final tree1 = createBinaryTree_1();
+  tree1.traverseInOrder(print);
+  sprt();
+  tree1.traversePreOrder(print);
+  sprt();
+  tree1.traversePostOrder(print);
+  sprt();
+  final tree2 = createBinaryTree_2();
+  print(tree2.height(tree2));
+  print(tree2);
+  final list = serialize(tree2);
+  final newTree = deserializeHelper(list);
+  print(newTree);
 }
 
-BinaryNode<int> createBinaryTree() {
+extension Serializable<T> on BinaryNode<T> {
+  void traversePreOrderWithNull(void Function(T? value) action) {
+    action(value);
+    if (leftChild == null) {
+      action(null);
+    } else {
+      leftChild!.traversePreOrderWithNull(action);
+    }
+    if (rightChild == null) {
+      action(null);
+    } else {
+      rightChild!.traversePreOrderWithNull(action);
+    }
+  }
+}
+
+List<T?> serialize<T>(BinaryNode<T> node) {
+  final list = <T?>[];
+  node.traversePreOrderWithNull((value) => list.add(value));
+  return list;
+}
+
+BinaryNode<T>? deserialize<T>(List<T?> list) {
+  if (list.isEmpty) return null;
+  final value = list.removeLast();
+  if (value == null) return null;
+
+  final node = BinaryNode<T>(value);
+  node.leftChild = deserialize(list);
+  node.rightChild = deserialize(list);
+  return node;
+}
+
+BinaryNode<T>? deserializeHelper<T>(List<T?> list) {
+  return deserialize(list.reversed.toList());
+}
+
+BinaryNode<int> createBinaryTree_2() {
+  final n15 = BinaryNode(15);
+  final n10 = BinaryNode(10);
+  final n5 = BinaryNode(5);
+  final n12 = BinaryNode(12);
+  final n25 = BinaryNode(25);
+  final n17 = BinaryNode(17);
+  n15.leftChild = n10;
+  n10.leftChild = n5;
+  n10.rightChild = n12;
+  n15.rightChild = n25;
+  n25.leftChild = n17;
+  return n15;
+}
+
+BinaryNode<int> createBinaryTree_1() {
   final zero = BinaryNode(0);
   final one = BinaryNode(1);
   final five = BinaryNode(5);
@@ -81,4 +144,8 @@ TreeNode<String> makeBeverageTree() {
   soda.add(gingerAle);
   soda.add(bitterLemon);
   return tree;
+}
+
+void sprt() {
+  print("=== separator ===");
 }
