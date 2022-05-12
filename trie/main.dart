81868705ef00bcc933fa79660cb217a0395624b6
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-12 18:30:41 +0300

    testing trie

--- trie/main.dart
+++ trie/main.dart
@@ -1,3 +1,12 @@
 import 'trie_node.dart';
+import 'string_trie.dart';
 
-void main() {}
+void main() {
+  final trie = StringTrie();
+  trie.insert("ascutevv");
+  if (trie.contains("cute")) {
+    print("cute is in the trie");
+  } else {
+    print('noooo');
+  }
+}
--- trie/string_trie.dart
+++ trie/string_trie.dart
@@ -2,4 +2,30 @@ import 'trie_node.dart';
 
 class StringTrie {
   TrieNode<int> root = TrieNode(key: null, parent: null);
+
+  void insert(String text) {
+    var current = root;
+
+    for (var codeUnit in text.codeUnits) {
+      current.children[codeUnit] ??= TrieNode(
+        key: codeUnit,
+        parent: current,
+      );
+      current = current.children[codeUnit]!;
+    }
+
+    current.isTerminating = true;
+  }
+
+  bool contains(String text) {
+    var current = root;
+    for (var codeUnit in text.codeUnits) {
+      final child = current.children[codeUnit];
+      if (child == null) {
+        return false;
+      }
+      current = child;
+    }
+    return current.isTerminating;
+  }
 }
