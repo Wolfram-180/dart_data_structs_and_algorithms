f4730a856887db08584753dedf73849ff0af7c71
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-12 19:27:36 +0300

    testing tries

--- trie/main.dart
+++ trie/main.dart
@@ -3,10 +3,18 @@ import 'string_trie.dart';
 
 void main() {
   final trie = StringTrie();
-  trie.insert("ascutevv");
-  if (trie.contains("cute")) {
-    print("cute is in the trie");
-  } else {
-    print('noooo');
-  }
+  trie.insert('car');
+  trie.insert('card');
+  trie.insert('care');
+  trie.insert('cared');
+  trie.insert('cars');
+  trie.insert('carbs');
+  trie.insert('carapace');
+  trie.insert('cargo');
+  print('Collections starting with "car"');
+  final prefixedWithCar = trie.matchPrefix('car');
+  print(prefixedWithCar);
+  print('\nCollections starting with "care"');
+  final prefixedWithCare = trie.matchPrefix('care');
+  print(prefixedWithCare);
 }
