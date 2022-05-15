d69540910fa484658eabc77428cade5d14282f14
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-15 16:09:23 +0300

    Dijkstra

--- graphs/dijkstra.dart
+++ graphs/dijkstra.dart
@@ -1,3 +1,4 @@
+import 'priority_queue.dart';
 import 'graph.dart';
 
 class Pair<T> extends Comparable<Pair<T>> {
@@ -13,4 +14,47 @@ class Pair<T> extends Comparable<Pair<T>> {
 
   @override
   String toString() => '($distance, $vertex)';
+}
+
+class Dijkstra<E> {
+  Dijkstra(this.graph);
+  final Graph<E> graph;
+
+  Map<Vertex<E>, Pair<E>?> shortestPaths(Vertex<E> source) {
+    final queue = PriorityQueue<Pair<E>>(priority: Priority.min);
+    final visited = <Vertex<E>>{};
+    final paths = <Vertex<E>, Pair<E>?>{};
+
+    for (final vertex in graph.vertices) {
+      paths[vertex] = null;
+    }
+
+    queue.enqueue(Pair(0, source));
+    paths[source] = Pair(0);
+    visited.add(source);
+
+    while (!queue.isEmpty) {
+      final current = queue.dequeue()!;
+      final savedDistance = paths[current.vertex]!.distance;
+      if (current.distance > savedDistance) continue;
+      visited.add(current.vertex!);
+      for (final edge in graph.edges(current.vertex!)) {
+        final neighbor = edge.destination;
+
+        if (visited.contains(neighbor)) continue;
+
+        final weight = edge.weight ?? double.infinity;
+        final totalDistance = current.distance + weight;
+
+        final knownDistance = paths[neighbor]?.distance ?? double.infinity;
+
+        if (totalDistance < knownDistance) {
+          paths[neighbor] = Pair(totalDistance, current.vertex);
+          queue.enqueue(Pair(totalDistance, neighbor));
+        }
+      }
+    }
+
+    return paths;
+  }
 }
--- graphs/main.dart
+++ graphs/main.dart
@@ -1,6 +1,7 @@
 import 'graph.dart';
 import 'breadth_first_search.dart';
 import 'depth_first_search.dart';
+import 'dijkstra.dart';
 
 void main() {
   /*
@@ -84,6 +85,7 @@ void main() {
   vertices.forEach(print);
   */
 
+/*
   final graph = AdjacencyList<String>();
   final a = graph.createVertex('A');
   final b = graph.createVertex('B');
@@ -96,4 +98,31 @@ void main() {
   graph.addEdge(c, d, edgeType: EdgeType.directed);
   print(graph);
   print(graph.hasCycle(a));
+*/
+
+  final graph = AdjacencyList<String>();
+  final a = graph.createVertex('A');
+  final b = graph.createVertex('B');
+  final c = graph.createVertex('C');
+  final d = graph.createVertex('D');
+  final e = graph.createVertex('E');
+  final f = graph.createVertex('F');
+  final g = graph.createVertex('G');
+  final h = graph.createVertex('H');
+  graph.addEdge(a, b, weight: 8, edgeType: EdgeType.directed);
+  graph.addEdge(a, f, weight: 9, edgeType: EdgeType.directed);
+  graph.addEdge(a, g, weight: 1, edgeType: EdgeType.directed);
+  graph.addEdge(g, c, weight: 3, edgeType: EdgeType.directed);
+  graph.addEdge(c, b, weight: 3, edgeType: EdgeType.directed);
+  graph.addEdge(c, e, weight: 1, edgeType: EdgeType.directed);
+  graph.addEdge(e, b, weight: 1, edgeType: EdgeType.directed);
+  graph.addEdge(e, d, weight: 2, edgeType: EdgeType.directed);
+  graph.addEdge(b, e, weight: 1, edgeType: EdgeType.directed);
+  graph.addEdge(b, f, weight: 3, edgeType: EdgeType.directed);
+  graph.addEdge(f, a, weight: 2, edgeType: EdgeType.directed);
+  graph.addEdge(h, g, weight: 5, edgeType: EdgeType.directed);
+  graph.addEdge(h, f, weight: 2, edgeType: EdgeType.directed);
+  final dijkstra = Dijkstra(graph);
+  final allPaths = dijkstra.shortestPaths(a);
+  print(allPaths);
 }
