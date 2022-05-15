acd0409892263559fb46e317d2e05291321f315b
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-15 10:32:19 +0300

    selection_sort

--- sorting/main.dart
+++ sorting/main.dart
@@ -1,8 +1,14 @@
 import 'bubble_sort.dart';
+import 'selection_sort.dart';
 
 void main() {
-  final list = [9, 4, 10, 3, 8];
+  var list = [9, 4, 10, 3];
   print('Original: $list');
   bubbleSort(list);
   print('Bubble sorted: $list');
+
+  list = [9, 4, 10, 3];
+  print('Original: $list');
+  selectionSort(list);
+  print('Selection sorted: $list');
 }
