157048942efa06f072a2951690241ff95ab9672f
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-15 15:28:17 +0300

    breadthFirstSearch

--- graphs/main.dart
+++ graphs/main.dart
@@ -1,6 +1,8 @@
 import 'graph.dart';
+import 'breadth_first_search.dart';
 
 void main() {
+  /*
   final graph = AdjacencyMatrix<String>();
   final singapore = graph.createVertex('Singapore');
   final tokyo = graph.createVertex('Tokyo');
@@ -32,4 +34,25 @@ void main() {
   for (final edge in graph.edges(sanFrancisco)) {
     print('${edge.source} to ${edge.destination}');
   }
+*/
+  final graph = AdjacencyList<String>();
+  final a = graph.createVertex('A');
+  final b = graph.createVertex('B');
+  final c = graph.createVertex('C');
+  final d = graph.createVertex('D');
+  final e = graph.createVertex('E');
+  final f = graph.createVertex('F');
+  final g = graph.createVertex('G');
+  final h = graph.createVertex('H');
+  graph.addEdge(a, b, weight: 1);
+  graph.addEdge(a, c, weight: 1);
+  graph.addEdge(a, d, weight: 1);
+  graph.addEdge(b, e, weight: 1);
+  graph.addEdge(c, f, weight: 1);
+  graph.addEdge(c, g, weight: 1);
+  graph.addEdge(e, h, weight: 1);
+  graph.addEdge(e, f, weight: 1);
+  graph.addEdge(f, g, weight: 1);
+  final vertices = graph.breadthFirstSearch(a);
+  vertices.forEach(print);
 }
