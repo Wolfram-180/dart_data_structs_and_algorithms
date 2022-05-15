b1fd168cf72d396b6a7dff06d1d4185684be22ef
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-15 12:41:56 +0300

    radix lexicographical order

--- sorting/main.dart
+++ sorting/main.dart
@@ -2,6 +2,7 @@ import 'bubble_sort.dart';
 import 'selection_sort.dart';
 import 'insertion_sort.dart';
 import 'merge_sort.dart';
+import 'radix_sort.dart';
 
 void main() {
   var list = [9, 4, 10, 3];
@@ -23,4 +24,27 @@ void main() {
   final sorted = mergeSort(list);
   print('Original: $list');
   print('Merge sorted: $sorted');
+
+  list = [88, 410, 1772, 20];
+  print("Original list: $list");
+  list.radixSort(); // LSD
+  print("Radix sorted: $list");
+
+  print(13.digits()); // 2
+  print(999.digits()); // 3
+  print(1345.digits()); // 4
+
+  print(1345.digitAt(0)); // 1
+  print(1345.digitAt(1)); // 3
+  print(1345.digitAt(2)); // 4
+  print(1345.digitAt(3)); // 5
+  print(1345.digitAt(4)); // null
+  print(1345.digitAt(5)); // null
+
+  list = [46, 500, 459, 1345, 13, 999];
+  print(list.maxDigits()); // 4
+
+  list = [46, 500, 459, 1345, 13, 999];
+  list.lexicographicalSort();
+  print(list);
 }
