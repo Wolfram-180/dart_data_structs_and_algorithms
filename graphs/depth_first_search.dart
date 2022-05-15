5ddaae83d242a033cf92e279a4adbbe98ce85f5e
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-15 15:58:19 +0300

    AdjacencyList

--- graphs/depth_first_search.dart
+++ graphs/depth_first_search.dart
@@ -30,3 +30,29 @@ extension DepthFirstSearch<E> on Graph<E> {
     return visited;
   }
 }
+
+extension CyclicGraph<E> on Graph<E> {
+  bool _hasCycle(Vertex<E> source, Set<Vertex<E>> pushed) {
+    pushed.add(source);
+
+    final neighbors = edges(source);
+    for (final edge in neighbors) {
+      if (!pushed.contains(edge.destination)) {
+        if (_hasCycle(edge.destination, pushed)) {
+          return true;
+        }
+      } else {
+        return true;
+      }
+    }
+
+    pushed.remove(source);
+
+    return false;
+  }
+
+  bool hasCycle(Vertex<E> source) {
+    Set<Vertex<E>> pushed = {};
+    return _hasCycle(source, pushed);
+  }
+}
--- graphs/main.dart
+++ graphs/main.dart
@@ -1,5 +1,6 @@
 import 'graph.dart';
 import 'breadth_first_search.dart';
+import 'depth_first_search.dart';
 
 void main() {
   /*
@@ -35,6 +36,8 @@ void main() {
     print('${edge.source} to ${edge.destination}');
   }
 */
+
+/*
   final graph = AdjacencyList<String>();
   final a = graph.createVertex('A');
   final b = graph.createVertex('B');
@@ -55,4 +58,42 @@ void main() {
   graph.addEdge(f, g, weight: 1);
   final vertices = graph.breadthFirstSearch(a);
   vertices.forEach(print);
+  */
+
+/*
+  final graph = AdjacencyList<String>();
+  final a = graph.createVertex('A');
+  final b = graph.createVertex('B');
+  final c = graph.createVertex('C');
+  final d = graph.createVertex('D');
+  final e = graph.createVertex('E');
+  final f = graph.createVertex('F');
+  final g = graph.createVertex('G');
+  final h = graph.createVertex('H');
+  graph.addEdge(a, b, weight: 1);
+  graph.addEdge(a, c, weight: 1);
+  graph.addEdge(a, d, weight: 1);
+  graph.addEdge(b, e, weight: 1);
+  graph.addEdge(c, g, weight: 1);
+  graph.addEdge(e, f, weight: 1);
+  graph.addEdge(e, h, weight: 1);
+  graph.addEdge(f, g, weight: 1);
+  graph.addEdge(f, c, weight: 1);
+
+  final vertices = graph.depthFirstSearch(a);
+  vertices.forEach(print);
+  */
+
+  final graph = AdjacencyList<String>();
+  final a = graph.createVertex('A');
+  final b = graph.createVertex('B');
+  final c = graph.createVertex('C');
+  final d = graph.createVertex('D');
+  graph.addEdge(a, b, edgeType: EdgeType.directed);
+  graph.addEdge(a, c, edgeType: EdgeType.directed);
+  graph.addEdge(c, a, edgeType: EdgeType.directed);
+  graph.addEdge(b, c, edgeType: EdgeType.directed);
+  graph.addEdge(c, d, edgeType: EdgeType.directed);
+  print(graph);
+  print(graph.hasCycle(a));
 }
