b00fae4c15824d6ff2975887d617073f13df38f9
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-15 11:32:20 +0300

    mergeSort

--- sorting/main.dart
+++ sorting/main.dart
@@ -1,5 +1,7 @@
 import 'bubble_sort.dart';
 import 'selection_sort.dart';
+import 'insertion_sort.dart';
+import 'merge_sort.dart';
 
 void main() {
   var list = [9, 4, 10, 3];
@@ -11,4 +13,14 @@ void main() {
   print('Original: $list');
   selectionSort(list);
   print('Selection sorted: $list');
+
+  list = [9, 4, 10, 3];
+  print('Original: $list');
+  insertionSort(list);
+  print('Insertion sorted: $list');
+
+  list = [7, 2, 6, 3, 9];
+  final sorted = mergeSort(list);
+  print('Original: $list');
+  print('Merge sorted: $sorted');
 }
